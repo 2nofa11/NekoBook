249b32809c594f6c083fa547a211da58edf45c1e
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-19 22:20:00 +0900

    p0 ネコ本写経スタート

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      msg: "bar",
+    };
+  },
+});
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
