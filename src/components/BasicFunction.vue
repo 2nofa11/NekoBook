145021883d3493d1093d9a5797d2d4bec01ab101
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-19 23:00:21 +0900

    p42 アニメーションもtransitionを使えば簡単

--- src/components/BasicFunction.vue
+++ src/components/BasicFunction.vue
@@ -1,10 +1,14 @@
 <template>
   <ol>
     <li v-for="item in list" :key="item">{{ item }}</li>
-    <button @click="handleClick">Click</button>
-    <p>{{ typeof message }}</p>
-    <input v-model.number="message" />
   </ol>
+  <button @click="handleClick">Click</button>
+  <p>{{ typeof message }}</p>
+  <input v-model.number="message" />
+  <transition class="parent">
+    <p v-if="show" class="child">Hello Vue.js</p>
+    <p v-else class="child">Hide</p>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -15,11 +19,13 @@ export default defineComponent({
     return {
       list: ["りんご", "ばなな", "いちご"] as string[],
       message: "" as string,
+      show: true,
     };
   },
   methods: {
     handleClick() {
-      alert(event?.target);
+      this.show = !this.show;
+      // alert(event?.target);
     },
   },
 });
@@ -28,5 +34,19 @@ export default defineComponent({
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+
+.child {
+  position: absolute;
 }
 </style>
