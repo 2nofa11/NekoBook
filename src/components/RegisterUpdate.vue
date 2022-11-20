b1c9abf0c411faaa1820b13a902b9e1b49acb75f
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-20 09:17:49 +0900

    p60 メソッド内のthisはインスタンスを指し、今回の場合は `RegisterUpdate`インスタンを指している

--- src/components/RegisterUpdate.vue
+++ src/components/RegisterUpdate.vue
@@ -3,6 +3,8 @@
     <div>{{ judgeLength }}</div>
     <div>{{ msg }}</div>
     <input type="text" :value="msg" />
+    <p>{{ count }}</p>
+    <button @click="increment">カウントを増やす</button>
   </div>
 </template>
 
@@ -12,12 +14,18 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return {
-      msg: "hello",
+      msg: "hello" as string,
+      count: 0 as number,
     };
   },
   computed: {
     judgeLength(): string {
       return this.msg.length >= 10 ? "t" : "f";
+    },
+  },
+  methods: {
+    increment: function () {
+      this.count += 1;
     },
   },
 });
