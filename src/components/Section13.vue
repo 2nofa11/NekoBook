4985f0b0916d7e9aece603d00b9d5f56a74c0714
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-23 16:24:57 +0900

    p98 フォーム入力の取得

--- src/components/Section13.vue
+++ src/components/Section13.vue
@@ -1,14 +1,28 @@
 <template>
   <button @click="handleClick($event)">ここの値</button>
+  <input :value="message" @input="handleInput($event)" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  data() {
+    return {
+      message: "" as string,
+    };
+  },
   methods: {
     handleClick(event: Event) {
       alert(`${(event.target as HTMLButtonElement).innerText}クリックしたよ！`);
+    },
+    handleInput(event: Event) {
+      const message = (event.target as HTMLInputElement).value;
+      if (message === "あああ") {
+        this.message = "";
+        return alert(message);
+      }
+      this.message = message;
     },
   },
 });
