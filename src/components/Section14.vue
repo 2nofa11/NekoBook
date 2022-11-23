8383415702deee25e3ee9a560b1d4c0f68bf428e
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-23 17:15:13 +0900

    p109 チェックボックス

--- src/components/Section14.vue
+++ src/components/Section14.vue
@@ -6,6 +6,13 @@
   <pre>{{ message }}</pre>
   <hr />
   <label> <input type="checkbox" v-model="value" />{{ value }} </label>
+  <br />
+  <div>
+    <label> <input type="checkbox" v-model="val" value="A" />A </label>
+    <label> <input type="checkbox" v-model="val" value="B" />B </label>
+    <label> <input type="checkbox" v-model="val" value="C" />C </label>
+    {{ val }}
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,6 +23,7 @@ export default defineComponent({
     return {
       message: "" as string,
       value: true as boolean,
+      val: [] as string[],
     };
   },
 });
