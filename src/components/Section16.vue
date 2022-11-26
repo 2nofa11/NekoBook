03f8f3d36d68b4ebe9ab1940c47226303029c91e
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-26 12:02:25 +0900

    p125 リストの絞り込みを算出プロパティにて実現 複雑な処理に便利

--- src/components/Section16.vue
+++ src/components/Section16.vue
@@ -17,6 +17,15 @@
     <li>{{ computedDataMethod() }}</li>
     <li>{{ computedDataMethod() }}</li>
   </ol>
+  <hr />
+  <ul>
+    <input v-model.number="budget" />円以下に絞り込む
+    <input v-model.number="limit" />件を表示
+    <p>{{ matched.length }}中{{ limited.length }}を表示</p>
+    <li v-for="item in limited" key="item.id">
+      {{ item.name }}：{{ item.price }}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -27,6 +36,15 @@ export default defineComponent({
     return {
       width: 800,
       height: 600,
+      limit: 1,
+      budget: 300,
+      lists: [
+        { id: 1, name: "オレンジ", price: 100 },
+        { id: 2, name: "バナナ", price: 200 },
+        { id: 3, name: "イチゴ", price: 400 },
+        { id: 4, name: "りんご", price: 500 },
+        { id: 5, name: "めろん", price: 600 },
+      ],
     };
   },
   methods: {
@@ -54,6 +72,12 @@ export default defineComponent({
     },
     computedData() {
       return Math.random();
+    },
+    matched() {
+      return this.lists.filter((el) => el.price < this.budget);
+    },
+    limited() {
+      return this.matched.slice(0, this.limit);
     },
   },
 });
