b26e0ea3868ae952d22a7e3f03d326c5e3814a79
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-30 07:32:59 +0900

    p185 共通処理を登録するMixin（今は非推奨）

--- src/components/Section26Parent.vue
+++ src/components/Section26Parent.vue
@@ -8,8 +8,20 @@
 import { defineComponent } from "vue";
 import Section26ChildA from "./Section26ChildA.vue";
 import Section26ChildB from "./Section26ChildB.vue";
+const mixin = {
+  created: function () {
+    this.hello();
+  },
+  methods: {
+    hello() {
+      console.log("created");
+    },
+  },
+};
+
 export default defineComponent({
   components: { Section26ChildA, Section26ChildB },
+  mixins: [mixin],
   data() {
     return {
       current: true,
