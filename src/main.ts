d660a3b3c9cefaa16b4c28bcd88c08f2d492cc99
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-02 07:44:51 +0900

    p236 自作したプラグインも登録できる

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import { store } from "./store/Section39_Store";
 
+const myPlugin = {
+  install(Vue: any, option: { lang: string }) {
+    console.log(option);
+  },
+};
 const app = createApp(App);
 app.use(store);
+app.use(myPlugin, { lang: "ja" });
 app.mount("#app");
