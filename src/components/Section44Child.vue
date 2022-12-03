37430c8beae6193161694f1dfa418eeb93b53527
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-03 13:09:25 +0900

    @266 バインドするヘルパーもある

--- src/components/Section44Child.vue
+++ src/components/Section44Child.vue
@@ -1,20 +1,22 @@
 <template>
-  <div><input type="text" v-model="message" /></div>
+  <!-- <div><input type="text" v-model="message" /></div> -->
+  <div><input type="text" @input="doUpdate" />{{ message }}</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapGetters } from "vuex";
 export default defineComponent({
-  computed: {
-    message: {
-      get() {
-        return this.$store.getters.message;
-      },
-      set(value) {
-        this.$store.dispatch("doUpdate", value);
-      },
-    },
-  },
+  computed: mapGetters(["message"]),
+  // message: {
+  //   get() {
+  //     return this.$store.getters.message;
+  //   },
+  //   set(value) {
+  //     this.$store.dispatch("doUpdate", value);
+  //   },
+  // },
+
   methods: {
     doUpdate(event: Event) {
       if (!(event.target instanceof HTMLInputElement)) return;
