15e4c8d288cea6b1b9297e6a26d43deca79c0bb2
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-03 12:36:05 +0900

    p262 vuex練習用のデータ作成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Section39 from "./components/Section39.vue";
+import Section39 from "./components/Section44.vue";
 </script>
 
 <template>
