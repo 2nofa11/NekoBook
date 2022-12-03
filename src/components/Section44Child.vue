7a7cb8e32dfdca15197c10c13d7d034b5a5a4b55
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-03 13:02:39 +0900

    p266 v-modelを使って上代管理をするとシンプルに書ける

--- src/components/Section44Child.vue
+++ src/components/Section44Child.vue
@@ -1,10 +1,20 @@
 <template>
-  <div><input type="text" @input="doUpdate" /></div>
+  <div><input type="text" v-model="message" /></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
+  computed: {
+    message: {
+      get() {
+        return this.$store.getters.message;
+      },
+      set(value) {
+        this.$store.dispatch("doUpdate", value);
+      },
+    },
+  },
   methods: {
     doUpdate(event: Event) {
       if (!(event.target instanceof HTMLInputElement)) return;
