11d9aed9d3c93e9123551de72ead77a94bf07149
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-06 23:03:19 +0900

    p286 $routeはマッチしたルートの情報が入ったオブジェクト （ルーティング内のメソッドはtoとfromを使うが内容は同じやつ）

--- src/components/Section48_View.vue
+++ src/components/Section48_View.vue
@@ -1,4 +1,8 @@
-<template>View</template>
+<template>
+  <div>View</div>
+  <div>{{ routerPath }}</div>
+  <div>{{ routerParam }}</div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -6,6 +10,14 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return {};
+  },
+  computed: {
+    routerPath() {
+      return this.$route.path;
+    },
+    routerParam() {
+      return this.$route.params;
+    },
   },
 });
 </script>
