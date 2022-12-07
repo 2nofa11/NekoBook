005579090da1738793fc9bc6162a38128a1078b4
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-08 07:45:57 +0900

    p291 paramsがある場合はname指定がわからん

--- src/components/Section50.vue
+++ src/components/Section50.vue
@@ -2,6 +2,10 @@
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink :to="`/view/${id}`" tag="button">View</RouterLink>
+    <RouterLink :to="{ path: '/view/1' }">View1</RouterLink>
+    <RouterLink :to="{ path: '/view/2', query: { id } }">View2</RouterLink>
+    <RouterLink :to="{ path: '/view/3', params: { id } }">View3</RouterLink>
+    <RouterLink :to="{ name: '', params: { id } }">View4</RouterLink>
   </nav>
   <RouterView />
 </template>
