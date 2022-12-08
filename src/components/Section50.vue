dccff0e2892e8fca1b96e3e6b2cc6934cc788c1e
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-09 08:28:59 +0900

    p292 exactいらないのでは？

--- src/components/Section50.vue
+++ src/components/Section50.vue
@@ -5,7 +5,7 @@
     <RouterLink :to="{ path: '/view/1' }">View1</RouterLink>
     <RouterLink :to="{ path: '/view/2', query: { id } }">View2</RouterLink>
     <RouterLink :to="{ path: '/view/3', params: { id } }">View3</RouterLink>
-    <RouterLink :to="{ name: '', params: { id } }">View4</RouterLink>
+    <RouterLink :to="{ path: '/hoge', params: { id } }">View4</RouterLink>
   </nav>
   <RouterView />
 </template>
