89969964565f0b7f5b09305ee743c6621293e6e2
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-10 08:36:58 +0900

    p292 プログラムによるンナビゲーションは「this.$router.push」を使う。 （replaceやgo）は使う機会あるのか、、、

--- src/components/Section50.vue
+++ src/components/Section50.vue
@@ -7,6 +7,7 @@
     <RouterLink :to="{ path: '/view/3', params: { id } }">View3</RouterLink>
     <RouterLink :to="{ path: '/hoge', params: { id } }">View4</RouterLink>
   </nav>
+  <button @click="change()">push</button>
   <RouterView />
 </template>
 
@@ -16,6 +17,11 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return { id: "hoge" };
+  },
+  methods: {
+    change() {
+      this.$router.push({ path: `/view/${this.id}`, query: { id: "query" } });
+    },
   },
 });
 </script>
