e6cdd863b5f9e4aacdf94df1faffbe51788ad5e8
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-10 10:03:57 +0900

    p298 コールバック関数むずいよ、、、、

--- src/components/Section51_Product.vue
+++ src/components/Section51_Product.vue
@@ -2,19 +2,35 @@
   <div>
     <h1>商品情報</h1>
     <p>コンページはID.{{ id }}の詳細を表示する</p>
+    <p>商品名：{{ item.name }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import { products, Product } from "./Section51_product";
 export default defineComponent({
-  data() {
-    return {};
-  },
   props: {
     id: {
       type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      item: {} as Product,
+    };
+  },
+  watch: {
+    id: {
+      handler() {
+        products.asyncFind(this.id, (item) => {
+          if (item === undefined) return;
+          this.item = item;
+        });
+      },
+      immediate: true,
     },
   },
 });
